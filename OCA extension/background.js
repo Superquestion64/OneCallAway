ce04fc4352017aae26747f1b72ae655ca6dd2726
BDDoylan <bddoylan@gmail.com>
2021-10-21 16:45:27 -0400

    Recent call key on main popup.html implementation.

--- OCA extension/background.js
+++ OCA extension/background.js
@@ -1,12 +1,22 @@
 function getRandomString(length) {
-    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let output = '';
-    for ( let i = 0; i < length; i++ ) {
-        output += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return output;
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let output = "";
+  for (let i = 0; i < length; i++) {
+    output += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return output;
+}
+
+function copyToClipboard(callKey) {
+    callKey.select();
+    this.document.execCommand("copy", true);
+    console.log(callKey + " is copied to clipboard");
 }
 
 chrome.commands.onCommand.addListener(() => {
-    console.log(getRandomString(20));
+  let key = getRandomString(20);
+  console.log(key);
+  chrome.storage.sync.set({ callKey: key });
+  copyToClipboard(key);
 });
+
