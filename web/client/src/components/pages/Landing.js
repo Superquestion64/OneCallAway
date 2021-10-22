542e8bf22bfb9cfc147cd90cf4da2129894738dd
Zhi Dong Dong <49508823+danny1233211@users.noreply.github.com>
2021-10-22 16:13:55 -0400

    life difficult

---
+++ web/client/src/components/pages/Landing.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+import {
+  Btn,
+  Container,
+  Divider,
+  FlexCentered,
+  StyledLink,
+  Title
+} from "../../styles/General.styled";
+import {
+  Feature,
+  FeatureList,
+  Icon,
+  LandingImage,
+  LandingInner,
+  Review,
+  Reviewer,
+  ReviewSection,
+  Section,
+  Stars,
+  Subtitle
+} from "./styles/Landing.styled";
+import Navbar from "../general/Navbar";
+
+const Landing = () => {
+  return (
+    <Container mh="100vh">
+      <Navbar landing={true} />
+      <LandingImage>
+        <LandingInner>
+          <FlexCentered>
+            <Title>One Call Away</Title>
+            <Subtitle>
+              Connect with friends and families anytime, anywhere
+            </Subtitle>
+            <StyledLink to="/signup">
+              <Btn
+                hoverColor="rgba(237, 66, 100, 1)"
+                br="1.8rem"
+                fs="1.5rem"
+                m="6rem 0 0 0"
+                bgColor="rgba(237, 66, 100, 0.9)"
+                p="0.8rem 1.5rem"
+                boxShadowColor="#fff">
+                Sign Up
+              </Btn>
+            </StyledLink>
+          </FlexCentered>
+        </LandingInner>
+      </LandingImage>
+      <Divider />
+      <Section bgColor="#fffde7">
+        <FlexCentered>
+          <Title fs="2.5rem" margin="0 0 1rem 0" textDecoration="underline">
+            Features
+          </Title>
+          <FeatureList name="features">
+            <Feature>
+              <Icon />
+              Invite friends with a simple link
+            </Feature>
+            <Feature>
+              <Icon />
+              Chat with anyone - anywhere
+            </Feature>
+            <Feature>
+              <Icon />
+              All for FREE!
+            </Feature>
+          </FeatureList>
+        </FlexCentered>
+      </Section>
+      <Divider />
+      <Section bgColor="#dcedc8" mb="1.1rem">
+        <Title fs="2.5rem" margin="1rem 0" textDecoration="underline">
+          What our users are saying...
+        </Title>
+        <FlexCentered>
+          <ReviewSection>
+            <FlexCentered>
+              <Stars>⭐ ⭐ ⭐ ⭐ ⭐</Stars>
+              <Review>
+                This site makes it crazy easy for me to hop in a call with my
+                bros during a game. What a revolutionary product!
+              </Review>
+              <Reviewer>- John Doe</Reviewer>
+            </FlexCentered>
+          </ReviewSection>
+        </FlexCentered>
+      </Section>
+    </Container>
+  );
+};
+
+export default Landing;
---
+++ web/client/src/components/pages/styles/Dashboard.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: ${({ h }) => h || "100%"};
+  width: ${({ w }) => w || "100%"};
+  margin: ${({ m }) => m || "0 auto"};
+  min-height: ${({ mh }) => mh || ""};
+  position: relative;
+  padding: ${({ p }) => p || ""};
+  background-color: ${({ bgColor }) => bgColor || "#fff"};
+  background-image: ${({ theme }) => theme.bg.dashboard};
+`;
