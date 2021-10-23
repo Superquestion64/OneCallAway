d19d2c6eeaf769b981ed6a532a0269e233e726b3
Sabrina Chen <chensabrina45@gmail.com>
2021-10-23 14:33:25 -0400

    JWT token authentication

--- backend/app.py
+++ backend/app.py
@@ -4,11 +4,14 @@ from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 import pymongo
 import uuid
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["JWT_SECRET_KEY"] = uuid.uuid4().hex
+jtw = JWTManager(app)
 
 #Database
 client = pymongo.MongoClient('mongodb://localhost:27017')
