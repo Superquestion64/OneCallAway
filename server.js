4e8fdca86c3d108f7514da1756285ef935eba8ba
BDDoylan <bddoylan@gmail.com>
2021-12-01 18:43:36 -0500

    heroku dep

--- server.js
+++ server.js
@@ -113,4 +113,7 @@ server.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+  }); 
 }
