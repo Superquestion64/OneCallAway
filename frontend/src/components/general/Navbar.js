81724f94216c6ae816c5fec94434a9e63b1c2d72
Zhi Dong Dong <49508823+danny1233211@users.noreply.github.com>
2021-12-09 00:38:24 -0500

    change logo redirection based off user status

--- frontend/src/components/general/Navbar.js
+++ frontend/src/components/general/Navbar.js
@@ -7,9 +7,13 @@ import {
 } from "./styles/Navbar.styled";
 import { Btn, Flex, FlexItem, StyledLink } from "../../styles/General.styled";
 import { Link } from "react-scroll";
+import { useSelector } from "react-redux";
 
 // navRoutes = [{path, title}]
 const Navbar = ({ navItems = [] }) => {
+  const { isAuthenticated } = useSelector(state => state.user);
+
+  const logoTo = isAuthenticated ? "/dashboard" : "/";
   const renderNavItems = navItems.map(navItem => {
     const { title, path } = navItem;
     if (title === "Features") {
@@ -37,7 +41,7 @@ const Navbar = ({ navItems = [] }) => {
         <PhoneIcon />
         <FlexItem fg="1">
           <SiteLogo>
-            <StyledLink to="/">OCA</StyledLink>
+            <StyledLink to={logoTo}>OCA</StyledLink>
           </SiteLogo>
         </FlexItem>
         <NavLinks>
