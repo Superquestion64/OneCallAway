7f9e747351066ad2425e279dd8e8f7542c04ef1c
BDDoylan <bddoylan@gmail.com>
2021-12-10 00:17:12 -0500

    visual fixes

--- frontend/src/styles/General.styled.js
+++ frontend/src/styles/General.styled.js
@@ -97,7 +97,7 @@ export const FlexCentered = styled.div`
   padding: ${({ p }) => p || ""};
   background-color: ${({ bgColor }) => bgColor || ""};
   border-radius: ${({ br }) => br || ""};
-  margin-left: ${({ ml }) => ml || ""}
+  margin-left: ${({ ml }) => ml || "35%"}
   flex-wrap: ${({fw}) => fw || ""}
 `;
 
